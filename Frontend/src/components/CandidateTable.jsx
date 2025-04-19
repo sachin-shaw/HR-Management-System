cc3e90e26037b67bfa03d5eb174f3ff038b2763f
Sachin Kumar <96615888+sachin-shaw@users.noreply.github.com>
2025-04-19 17:39:25 +0530

    Update CandidateTable.jsx

--- Frontend/src/components/CandidateTable.jsx
+++ Frontend/src/components/CandidateTable.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "./candidateTable.css";
+import "./CandidateTable.css";
 
 function CandidateTable() {
   const [candidates, setCandidates] = useState([]);
